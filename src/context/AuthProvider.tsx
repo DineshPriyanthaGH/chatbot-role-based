70e1d82c4b82189b2979e5a87011b794fd6bd5b2
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-06-25 07:27:09 +0530

    authprovider function

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,3 +1,16 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import { auth } from '../firebase';
 import { onAuthStateChanged, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from 'firebase/auth';
+
+
+
+const AuthContext = createContext<any>(null);
+ 
+export const AuthProvider = ({
+    children } :  any) => {
+    const [user, setUser] = useState<any>(null);
+
+    
+
+    }
+
