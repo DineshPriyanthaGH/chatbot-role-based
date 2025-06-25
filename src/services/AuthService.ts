10ca48f674dae000af9ea2a602930b9a11a2c55c
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-06-25 08:17:40 +0530

    authservice.ts

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -2,4 +2,16 @@ import { auth } from '../firebase'; // Firebase config
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 
+export const signUpUser = async (email: string, password: string) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCredential.user; // Return the user object
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error('Sign up failed: ' + error.message);
+    } else {
+      throw new Error('Sign up failed: Unknown error');
+    }
+  }
+};
 
